fbf9c06d197c2c224a1aa51a10ed501df364c3b3
Gennadii Remizov <gennadii.remisov@u-charged.com>
2018-01-07 10:05:28 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,44 @@
+/**
+ * Created by remgv on 05.01.18.
+ */
+$(document).ready(function () {
+   'use strict';
+    // console.log('paperjs');
+
+    paper.install(window);
+    paper.setup(document.getElementById('mainCanvas'));
+
+    // const d = Shape.Circle(200, 200, 50);
+    // d.fillColor = 'green';
+
+    // var c;
+    // for (let x = 25; x < 400; x += 50) {
+    //     for (let y = 25; y < 400; y += 50) {
+    //         c = Shape.Circle(x, y, 20);
+    //         c.fillColor = 'blue';
+    //     }
+    // }
+
+    // var tool = new Tool();
+    //
+    // tool.onMouseDown = function (event) {
+    //     var c = Shape.Circle(event.point.x, event.point.y, 20);
+    //     c.fillColor = 'blue';
+    // };
+
+    // var tool = new Tool();
+    //
+    // tool.onMouseDown = function (event) {
+    //     var c = Shape.Circle(event.point.x, event.point.y, 20);
+    //     c.fillColor = 'blue';
+    // };
+    var c = Shape.Circle(200, 200, 80);
+    c.fillColor = 'green';
+    var text = new PointText(200, 200);
+    text.justification = 'center';
+    text.fillColor = 'white';
+    text.fontSize = 20;
+    text.content = 'hello world';
+
+    paper.view.draw();
+});
